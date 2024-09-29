b6bbcf6796d72e4f9049da8bfc20ea51ebd5f340
devendra <devedndras.bca2022@ssism.org>
2024-09-29 21:37:09 +0530

    login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react' 
+import { Outlet, RouterProvider } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+ <Header/>
+ <Outlet/>
+ <Footer/>
     </>
   )
 }
