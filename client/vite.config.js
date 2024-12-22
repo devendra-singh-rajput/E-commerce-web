d98adcc8638c7ff5e47be432695025ccfbf58d90
devendra <devedndras.bca2022@ssism.org>
2024-12-22 15:58:27 +0530

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'dist', // Default output directory for Vite
+  },
   // this is add for hostin at local network
   server: {
     host: '0.0.0.0', // This allows access from other devices on the local network
