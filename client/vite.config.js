674a5fae673f035bd26c62568afcbc693b63e3a1
devendra <devedndras.bca2022@ssism.org>
2024-12-26 15:03:26 +0530

    redirect

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
   },
   // this is add for hostin at local network
   server: {
+    historyApiFallback: true,
     host: '0.0.0.0', // This allows access from other devices on the local network
     port: 5173, // You can choose any port
 },
+base: '/',
 })
